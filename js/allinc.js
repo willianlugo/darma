6fdfc29e15abe1125831c6dc18d263db6675025d
Will Yamaura <willianyamaura@hotmail.com>
2019-02-16 21:30:25 -0300

    enviando

---
+++ js/allinc.js
@@ -0,0 +1,7 @@
+var incFiles = '<script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>';
+incFiles    += '<script type="text/javascript" src="js/functions.js"></script>';
+incFiles    += '<script type="text/javascript" src="js/ie10-viewport-bug-workaround.js"></script>';
+incFiles    += '<script type="text/javascript" src="js/holder.min.js"></script>';
+incFiles    += '<script type="text/javascript" src="js/popper.min.js"></script>';
+incFiles    += '<script type="text/javascript" src="js/bootstrap.min.js"></script>';
+document.write(incFiles);
